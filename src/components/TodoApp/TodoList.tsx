4b861fce9396b263a8bff443630da699f0ad81a2
HarryXiong24 <harryxiong24@gmail.com>
2021-11-24 19:57:39 +0800

    11.24

--- src/components/TodoApp/TodoList.tsx
+++ src/components/TodoApp/TodoList.tsx
@@ -1,5 +1,39 @@
+import { List } from 'antd';
+import React from 'react';
+import './TodoList.scss';
+
 const TodoList: React.FC = () => {
-  return null;
+  const data = [
+    {
+      title: 'Ant Design Title 1',
+    },
+    {
+      title: 'Ant Design Title 2',
+    },
+    {
+      title: 'Ant Design Title 3',
+    },
+    {
+      title: 'Ant Design Title 4',
+    },
+  ];
+  return (
+    <div>
+      <List
+        className="todo-list"
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item>
+            <List.Item.Meta
+              title={<a href="https://ant.design">{item.title}</a>}
+              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+            />
+          </List.Item>
+        )}
+      />
+    </div>
+  );
 };
 
 export default TodoList;
