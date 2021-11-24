f89609fc2c6d60e7a15e66790e7deb2f957c950d
harryxiong <harryxiong24@gmail.com>
2021-11-24 11:06:06 +0800

    11.24

---
+++ src/components/TodoApp/index.tsx
@@ -0,0 +1,5 @@
+const TodoApp: React.FC = () => {
+  return null;
+};
+
+export default TodoApp;
--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '@/assets/logo.svg';
+import TodoApp from '@/components/TodoApp/index';
 import './index.scss';
 
 const Todo: React.FC = (): JSX.Element => {
@@ -7,6 +8,13 @@ const Todo: React.FC = (): JSX.Element => {
     <div className="todo">
       <img src={logo} className="todo-logo" alt="logo" />
       <p className="todo-title">Xbear Todolist</p>
+      <TodoApp />
+      <a
+        href="https://github.com/HarryXiong24/react-hooks-todolist"
+        className="todo-footer"
+      >
+        <strong>Copyright (c) 2021 Harry Xiong</strong>
+      </a>
     </div>
   );
 };
